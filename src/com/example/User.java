6e4db554f0f0db5290ab957cb87d6c5305e924c8
MERIN BABU <128206564+Merin12333@users.noreply.github.com>
2025-11-14 10:44:42 +0530

    Add files via upload

---
+++ src/com/example/User.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class User {
+	public static void main(String[] args) {
+		EmployeeBean eb=new EmployeeBean();
+		eb.setEid(101);
+		eb.setEname("Merin");
+		eb.setEsal(10000);
+		Gson obj=new Gson();
+		String str=obj.toJson(eb);
+		System.out.println(str);
+	}
+
+}
